db2971c0a9ab357b300847a4555fc5a7419ca61e
Saranya <jsaran.bj@gmail.com>
2024-02-29 09:56:28 +0100

    Create a class with basic requirement for the expense tracker

--- run.py
+++ run.py
@@ -14,3 +14,26 @@ SHEET=GSPREAD_CLIENT.open('Expense tracker')
 expenses = SHEET.worksheet('Expenses')
 datas=expenses.get_all_values()
 print(datas)
+
+class Expense:
+    def __init__(self,amount,category,details="",date_time=None):
+        self.amount=amount
+        self.category=category
+        self.details=details
+        self.date_time=date_time
+
+
+
+def get_expense_of_user():
+    """
+    Get the expenses form the user
+    amount:int, category:string, details:string,date/time:date
+    """
+    cost = float(input("Enter the amount which you spent:"))
+    print(f"you have entered the {cost}")
+    expense_name=input("Enter the Category of expense:")
+    print(f"You have entered{expense_name}")
+    
+
+
+get_expense_of_user()
