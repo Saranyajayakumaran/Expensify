9c8318e0ec21998c29e151b768a2190515d42499
Saranya <jsaran.bj@gmail.com>
2024-02-29 11:54:48 +0100

    Add options to user to select and printerror if wrong option selected

--- run.py
+++ run.py
@@ -30,10 +30,71 @@ def get_expense_of_user():
     amount:int, category:string, details:string,date/time:date
     """
     cost = float(input("Enter the amount which you spent:"))
-    print(f"you have entered the {cost}")
-    expense_name=input("Enter the Category of expense:")
-    print(f"You have entered{expense_name}")
+    print(f"you have entered : {cost} euros")
+
+    categories=["Food","Rent","Fun","Car","Miscellenious"]
+
+    while True: 
+        for i , name_of_category in enumerate(categories,1):
+            print(f" {i}. {name_of_category}")
+        category_name=input("Select a category:",i)
+        print(f"you selcted:{category_name}")
+        break
     
+def  show_all_expense():
+    print("All the expenses")
+    
+def summerize_expenses():
+    print("Expense summerize")
+
+def delete_expense():
+    print("expense deleted")
 
 
-get_expense_of_user()
+def menu_options():
+    options=["Add the expense","Show all the expense","Summerize expense","Delete expense","Exit"]
+
+    while True:
+        print("****Options****")
+        print()
+        for opt, option in enumerate(options,1):
+           print(f"{opt}.{option}")
+        try:
+            value_range=f"[1-{len(options)}]"
+            user_choice=int(input(f"Select an option {value_range}:"))
+            print(f"you ave selected:  {user_choice}")
+
+    
+            if user_choice in range(len(options)+1):
+                if user_choice== 1:
+                    get_expense_of_user()
+                elif user_choice== 2:
+                    show_all_expense()
+                elif user_choice== 3:
+                    summerize_expenses()
+                elif user_choice== 4:
+                    delete_expense()
+                elif user_choice== 5:
+                    print("Exting.....")
+                    break
+
+                else:
+                    print()
+                    print("Invalid option, try again")
+                    print("---------------------------")
+                    print()
+            else:
+                print()
+                print("Invalid option, try again")
+                print("----------------------------")
+                print()
+        except:
+            print()
+            print(f"Invalid Input ,Please enter a number from {value_range}")
+            print()
+
+def main():
+    menu_options() 
+
+
+main()
